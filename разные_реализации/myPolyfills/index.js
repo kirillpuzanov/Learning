6612a77244a2647e1d5d32024458ff4e4a056b94
s0369 <pko@liis.su>
2024-05-01 18:29:12 +0300

    add my map polyfill

--- разные_реализации/myPolyfills/index.js
+++ разные_реализации/myPolyfills/index.js
@@ -1,1 +1,24 @@
-module.exports = function () {}
+module.exports = function () {
+
+  if(!Array.prototype.myMap) {
+    Array.prototype.myMap = function(callback) {
+      if(typeof callback !== 'function') {
+          throw new TypeError('callback mast be a function')
+      }
+      if(!Array.isArray(this) && typeof this !== 'string') {
+        throw new TypeError('myMap is not a function')
+      }
+      if(this.length === 0 || !this.length) {
+        return [];
+      }
+
+      const result = [];
+      for(let i = 0; i < this.length; i++) {
+        result.push(callback(this[i], i, this));
+      }
+      return result;
+    }
+  }
+
+
+}
--- разные_реализации/myPolyfills/polyfills.test.js
+++ разные_реализации/myPolyfills/polyfills.test.js
@@ -1,6 +1,6 @@
 const expect = require('chai').expect;
 
-const myPolyfills = require('./polyfills');
+const myPolyfills = require('./index');
 
 describe('Test myPolyfills', () => {
   before(() => {
@@ -9,9 +9,19 @@ describe('Test myPolyfills', () => {
 
   describe('test map', () => {
 
-    it('Check callback in args', () => {
-        const badFn = () => [1,2,3].myMap();
-        expect(badFn).to.throw(TypeError);
+    it('Check call without callback in args', () => {
+      const badFn = () => [1, 2, 3].myMap();
+      expect(badFn).to.throw(TypeError);
+    });
+
+    it('call from boolean', () => {
+      const badFn = () => true.myMap((el) => el * 2);
+      expect(badFn).to.throw(TypeError);
+    })
+
+    it('compare with map', () => {
+      expect([1, 2, 3, 4, 5].myMap((el) => el * 2)).to.deep
+        .equal([2, 4, 6, 8, 10]);
     })
 
   })
